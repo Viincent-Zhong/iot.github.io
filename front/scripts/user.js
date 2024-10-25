c30c565824a173507ae3a6046d40f4ac5ffe250f
MayaHill <71844243+MayaHill@users.noreply.github.com>
2024-10-25 23:11:10 +0200

    [Server] Fixed Dockerfile: didn't install everything properly [Front] Added login page and redirection to default device page using debug data

---
+++ front/scripts/user.js
@@ -0,0 +1,48 @@
+const deviceContainer = document.getElementById('deviceContainer');
+
+deviceContainer.innerHTML = "";
+
+for (const device of user_data["devices"]) {
+    const deviceDiv = document.createElement('div');
+    deviceDiv.className = 'device-container';
+
+    deviceDiv.innerHTML = `
+        <div class='device-card'> 
+            <img src='assets/d1_mini_pro.png' alt='Avatar' style='width:100%'>
+            <div class='device-card-info'> 
+                <p>ID: ${device["id"]}
+                <br>Type: ${device["type"]}
+                <br>Status: ${device["status"]}</p>
+            </div>
+        </div>
+        <div class='device-chart'>
+            <canvas id='chart-${device["id"]}'></canvas>
+        </div>
+    `;
+
+    deviceContainer.appendChild(deviceDiv);
+
+    let dates = device["data"].map(item => item.date);
+    let values = device["data"].map(item => item.value);
+
+    const canvas = deviceDiv.querySelector(`#chart-${device["id"]}`);
+    const ctx = canvas.getContext('2d');
+
+    let myChart = new Chart(ctx, {
+        type: "line",
+        data: {
+            labels: dates,
+            datasets: [{
+                label: `Device ${device["id"]}`,
+                data: values,
+                borderColor: 'blue',
+                borderWidth: 2,
+                fill: true,
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false
+        }
+      });
+}
---
+++ front/scripts/user_data.js
@@ -0,0 +1,45 @@
+var user_data = {
+    "devices": [
+        {
+            "id": "k1928JJ92",
+            "type": "D1 Mini Pro",
+            "status": "On",
+            "data": [
+                { "date": "Oct 20 2024 6:00", "value": 0.1 },
+                { "date": "Oct 20 2024 12:00", "value": 0.9 },
+                { "date": "Oct 20 2024 18:00", "value": 0.4 },
+                { "date": "Oct 21 2024 3:00", "value": 0.0 },
+                { "date": "Oct 21 2024 9:00", "value": 0.6 },
+                { "date": "Oct 21 2024 15:00", "value": 0.8 },
+                { "date": "Oct 21 2024 21:00", "value": 0.2 },
+                { "date": "Oct 22 2024 7:00", "value": 0.3 },
+                { "date": "Oct 22 2024 13:00", "value": 1.0 },
+                { "date": "Oct 22 2024 19:00", "value": 0.5 },
+                { "date": "Oct 23 2024 2:00", "value": 0.0 },
+                { "date": "Oct 23 2024 8:00", "value": 0.4 },
+                { "date": "Oct 23 2024 14:00", "value": 0.9 },
+                { "date": "Oct 23 2024 20:00", "value": 0.3 },
+                { "date": "Oct 24 2024 5:00", "value": 0.0 },
+                { "date": "Oct 24 2024 11:00", "value": 0.7 },
+                { "date": "Oct 24 2024 17:00", "value": 0.6 },
+                { "date": "Oct 24 2024 23:00", "value": 0.2 }
+            ]
+        },
+        {
+        "id": "18h1AS181",
+        "type": "D1 Mini Pro",
+        "status": "Off",
+        "data": [
+            { date: "Oct 20 2024 08:15", value: 0.0 },
+            { date: "Oct 20 2024 12:30", value: 0.6 },
+            { date: "Oct 20 2024 16:45", value: 0.3 },
+            { date: "Oct 21 2024 09:00", value: 0.1 },
+            { date: "Oct 21 2024 13:00", value: 0.8 },
+            { date: "Oct 21 2024 17:30", value: 0.2 },
+            { date: "Oct 22 2024 10:15", value: 0.4 },
+            { date: "Oct 22 2024 14:20", value: 0.9 },
+            { date: "Oct 22 2024 18:00", value: 0.5 }
+            ]
+        }
+    ]
+}
